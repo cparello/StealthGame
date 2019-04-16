8211df9c6b0d283f0804833128c1d302ff5bae98
chris <christopherparello@gmail.com>
2019-04-15 20:07:19 -0700

    Extraction zone setup

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(20.0f));
+	RootComponent = OverlapComp;
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandledOverlap);
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(200.0f, 200.0f, 200.0f);
+	DecalComp->SetupAttachment(RootComponent);
+}
+
+
+void AFPSExtractionZone::HandledOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlapped with extraction!"))
+}
+
+
